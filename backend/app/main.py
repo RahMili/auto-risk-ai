ffe7cf7b88e15ff0a67d3e9d2a5a89750e19928f
Mili <rahul.mili@capgemini.com>
2026-04-01 17:18:50 +0530

    adding prod env changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",         # local dev
+        "http://your-ec2-public-ip",     # production
+    ],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
